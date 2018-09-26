de1e1a5f166a73a0520365d4799399b35ebccddd
Dwight Townsend <townsenddw@gmail.com>
2018-09-26 11:09:26 -0700

    Cleaned up env.py

--- jhubctl/env.py
+++ jhubctl/env.py
@@ -2,4 +2,3 @@
 ROLE_TEMPLATE_URL = "https://amazon-eks.s3-us-west-2.amazonaws.com/cloudformation/2018-08-30/amazon-eks-service-role.yaml"
 VPC_TEMPLATE_URL = "https://amazon-eks.s3-us-west-2.amazonaws.com/cloudformation/2018-08-30/amazon-eks-vpc-sample.yaml"
 NODE_TEMPLATE_URL = "https://amazon-eks.s3-us-west-2.amazonaws.com/cloudformation/2018-08-30/amazon-eks-nodegroup.yaml"
-SPOT_TEMPLATE_URL = "https://raw.githubusercontent.com/townsenddw/k8s-eks-deploy/master/spot-nodes.yaml"
